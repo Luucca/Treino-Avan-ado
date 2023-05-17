a8b0c048def6d454879b52b68db7ce79b6cd776f
luucca <lucas001santosdias@gmail.com>
2023-05-17 12:29:33 -0300

    Forever

--- 05/01.js
+++ 05/01.js
@@ -4,7 +4,6 @@ document.addEventListener(  "keydown",(event) => {
     const keyName = event.key;
 
     if (keyName === "f") {
-      // do not alert when only Control key is pressed.
       return alert('Ola')
     }
   }
